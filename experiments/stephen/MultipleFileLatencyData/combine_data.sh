3a0170b4d7da3ffe050dd61a0c740b7a29f5f3df
root <root@ip-128-84-105-215.localdomain>
2015-12-16 21:14:46 +0000

    Updated combine data

--- experiments/stephen/MultipleFileLatencyData/combine_data.sh
+++ experiments/stephen/MultipleFileLatencyData/combine_data.sh
@@ -1,10 +1,9 @@
 clients=(1 2 4 8 16 32 64)
-ratios=(1.0 0.25 0.5 0.75 0.0)
+ratios=(1.0 0.0)
 for c in $(seq 0 6)
 do
-	for r in $(seq 0 4)
+	for r in $(seq 0 1)
 	do
-		cat ${clients[c]}_${ratios[r]}/temp*
 		cat ${clients[c]}_${ratios[r]}/temp* >> ${clients[c]}_${ratios[r]}.csv
 		rm -f ${clients[c]}_${ratios[r]}/temp*
 	done
