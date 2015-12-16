1331bac437ccca4a6692230a87d9aace9e88ef82
root <root@ip-128-84-105-169.localdomain>
2015-12-16 21:13:36 +0000

    Updated make folder

--- experiments/stephen/MultipleFileLatencyData/make_folders.sh
+++ experiments/stephen/MultipleFileLatencyData/make_folders.sh
@@ -1,8 +1,8 @@
 clients=(1 2 4 8 16 32 64)
-ratios=(1.0 0.25 0.5 0.75 0.0)
+ratios=(1.0 0.0)
 for c in $(seq 0 6)
 do
-	for r in $(seq 0 4)
+	for r in $(seq 0 1)
 	do
 		mkdir ${clients[c]}'_'${ratios[r]}
 	done
