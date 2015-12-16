7609022edce581e92b1cf0403747c1cfdefbc962
Stephen Chan <stephenchan@Stephens-MacBook-Air.local>
2015-12-16 15:58:20 -0500

    Updated file names in Multiple file latency experiments

--- experiments/stephen/MultipleFileLatencyExperiments/single_experiment.sh
+++ experiments/stephen/MultipleFileLatencyExperiments/single_experiment.sh
@@ -16,7 +16,7 @@ rw_ratio=$2
 node_id=$3
 
 #Output file
-output_filename='hdfsrs/experiments/stephen/MultipleFileThroughputData/'$total_clients'_'$rw_ratio'/'
+output_filename='hdfsrs/experiments/stephen/MultipleFileLatencyData/'$total_clients'_'$rw_ratio'/'
 
 #If more than two jobs, split them across the machines
 if [ $total_clients -gt 1 ]
@@ -33,5 +33,5 @@ requests_per_client=$(($requests_per_machine / $clients_per_machine))
 
 for i in $(seq 1 $clients_per_machine)
 do
-	nohup hadoop jar hdfsrs/experiments/stephen/MultipleFileThroughputExperiments/MultipleFileThroughput.jar run $requests_per_client 32 $rw_ratio '/node'$node_id'file'$i'.txt' &>> $output_folder'temp'$i&
+	nohup hadoop jar hdfsrs/experiments/stephen/MultipleFileLatencyExperiments/MultipleFileLatency.jar run $requests_per_client 32 $rw_ratio '/node'$node_id'file'$i'.txt' &>> $output_folder'temp'$i&
 done
