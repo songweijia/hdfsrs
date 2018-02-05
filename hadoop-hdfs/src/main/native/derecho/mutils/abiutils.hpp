1bf7685e58f1f46c784e3406355b0f3c3b8aeab5
Theodoros Gkountouvas <tedgoud@cs.cornell.edu>
2018-02-05 09:52:55 -0500

    Removed Derecho.

--- hadoop-hdfs/src/main/native/derecho/mutils/abiutils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <exception>
-#include <string>
-
-namespace mutils{
-	
-	std::string exn_typename(std::exception_ptr p);
-
-}
--- hadoop-hdfs/src/main/native/derecho/update-submodules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Shortcut to update both submodules to their latest version
-
-git submodule foreach git pull origin master
-git add rdmc sst
